870426f510193f85dbc5bab0b0f4054873a08075
who4m1 <yoon.assessoria@gmail.com>
2021-06-27 12:47:05 -0300

    atualizando o backend

--- src/route.js
+++ src/route.js
@@ -1,13 +1,17 @@
 const express = require('express')
+const QuestionController = require('./controllers/QuestionController')
+const RoomController = require('./controllers/RoomController')
 
 const route = express.Router()
 
 //Route definitions
-route.get('/', (req, res) => res.render("index"))
-route.get('/room', (req, res) => res.render("room"))
-route.get('/create-pass', (req, res) => res.render("create-pass"))
+route.get('/', (req, res) => res.render("index", {page: 'enter-room'}))
+route.get('/create-pass', (req, res) => res.render("index", {page: 'create-pass'}))
+
+route.get('/room/:room', (req, res) => res.render("room"))
 
 //Format
-//route.post('/room/:room/:question/:action',)
+route.post('/question/:room/:question/:action', QuestionController.index)
+route.post('/create-room', RoomController.create)
 
 module.exports = route
